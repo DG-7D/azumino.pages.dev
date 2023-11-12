b0a082df8a37d3aa78185623fb1fdd9d8b7b3e04
DG-7D <96519093+DG-7D@users.noreply.github.com>
2023-11-12 18:06:03 +0900

    Imageを使うようにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://azumino.pages.dev/",
-    markdown: {
-        shikiConfig: {
-            theme: "github-dark",
-            // theme: "css-variables",
-            wrap: true,
-        }
+  site: "https://azumino.pages.dev/",
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark",
+      // theme: "css-variables",
+      wrap: true
     }
+  },
+  integrations: [mdx()]
 });
