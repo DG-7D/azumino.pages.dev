f20bb24dd9f88abe2131223e5aad16724e06f2b7
DG-7D <96519093+DG-7D@users.noreply.github.com>
2023-12-17 11:50:48 +0900

    _サイトマップを生成するようにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://azumino.pages.dev/",
-    markdown: {
-        shikiConfig: {
-            theme: "github-dark",
-            // theme: "css-variables",
-            wrap: true
-        },
-        rehypePlugins: [[rehypeExternalLinks, {
-            target: "_blank",
-            rel: []
-        }]]
+  site: "https://azumino.pages.dev/",
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark",
+      // theme: "css-variables",
+      wrap: true
     },
-    integrations: [mdx()]
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: "_blank",
+      rel: []
+    }]]
+  },
+  integrations: [mdx(), sitemap()]
 });
